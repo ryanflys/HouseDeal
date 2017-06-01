941eca0cb601940e4ef8fef80964fce62315f253
flyfish <guozhiyong@yanzhuanjia.cn>
2017-06-01 20:31:35 +0800

    add admin indexController

---
+++ app/Http/Controllers/admin/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/1
+ * Time: 20:19
+ */
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+class adminController extends CommonController
+{
+    
+}
